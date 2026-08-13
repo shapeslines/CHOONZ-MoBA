efc5c121717b943cf9ae024b43d043eb78a35069
Carson <carsongpacker@gmail.com>
2026-08-13 02:28:13 -0400

    security: reject malformed test harness options

--- tests/test.h
+++ tests/test.h
@@ -54,9 +54,19 @@ inline int run_all(int argc, char** argv) {
     const char* filter = nullptr;   // --filter <substr>: substring of "suite.name"
     bool list = false;              // --list:           print cases, run nothing
     for (int i = 1; i < argc; ++i) {
-        if      (arg_eq(argv[i], "--suite")  && i + 1 < argc) suite  = argv[++i];
-        else if (arg_eq(argv[i], "--filter") && i + 1 < argc) filter = argv[++i];
-        else if (arg_eq(argv[i], "--list")) list = true;
+        if (arg_eq(argv[i], "--suite") || arg_eq(argv[i], "--filter")) {
+            if (i + 1 >= argc || !argv[i + 1] || argv[i + 1][0] == '-') {
+                std::printf("ERROR: %s requires a selector value\n", argv[i]);
+                return 2;
+            }
+            if (arg_eq(argv[i], "--suite")) suite = argv[++i];
+            else                               filter = argv[++i];
+        } else if (arg_eq(argv[i], "--list")) {
+            list = true;
+        } else {
+            std::printf("ERROR: unknown test harness option '%s'\n", argv[i]);
+            return 2;
+        }
     }
 
     int n_run = 0, n_failed = 0;
